d473f280788fa751fc447a91afbfd0908ed9c0c5
Marielle Beusch <mariellebeusch@me.com>
2020-11-11 17:06:45 +0100

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+      <input v-model="forecast">
+      <br>
+      {{ forecast }}
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      city: '',
+      forecast: '20 Grad, Sonne'
+    }
+  },
+  methods: {
+    fetchWeather () {
+      // fetch api
+      // später axios
+
+    }
+  }
+}
+</script>
